c5c02735536a3512eaa4550905d14a384559512f
NguyenKim-web <n.k.kim868@gmail.com>
2024-07-28 10:13:07 +0900

    them thu muc

---
+++ admin/slider.php
@@ -0,0 +1,25 @@
+<div class="admin-content">
+    <!-- left side -->
+    <div class="admin-content-left">
+        <ul>
+            <li><a href="#">Danh Muc</a>
+                <ul>
+                    <li><a href="">Them danh muc</a></li>
+                    <li><a href="">Danh sach danh muc</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Loai san pham</a>
+                <ul>
+                    <li><a href="">Them loai san pham</a></li>
+                    <li><a href="">Danh sach loai san pham</a></li>
+                </ul>
+            </li>
+            <li><a href="#">San pham</a>
+                <ul>
+                    <li><a href="">Them san pham</a></li>
+                    <li><a href="">Danh sach san pham</a></li>
+                </ul>
+            </li>
+        </ul>
+
+    </div>
