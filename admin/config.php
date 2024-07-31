d0a64497240c8cdf41d4513f63972ea70dd8c91c
NguyenKim-web <n.k.kim868@gmail.com>
2024-07-31 22:57:56 +0900

    first commit

---
+++ admin/config.php
@@ -0,0 +1,9 @@
+<!-- Config -->
+
+<?php
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "nguyenkim1");
+    define("DB_NAME", "web_banhang_v1");
+    // define("DB_PORT", 3307);
+?>
---
+++ admin/slider.php
@@ -0,0 +1,25 @@
+<div class="admin-content">
+    <!-- left side -->
+    <div class="admin-content-left">
+        <ul>
+            <li><a href="">Danh Muc</a>
+                <ul>
+                    <li><a href="category_add.php">Them danh muc</a></li>
+                    <li><a href="category_list.php">Danh sach danh muc</a></li>
+                </ul>
+            </li>
+            <li><a href="">Loai san pham</a>
+                <ul>
+                    <li><a href="brand_add.php">Them loai san pham</a></li>
+                    <li><a href="brand_list.php">Danh sach loai san pham</a></li>
+                </ul>
+            </li>
+            <li><a href="">San pham</a>
+                <ul>
+                    <li><a href="product_add.php">Them san pham</a></li>
+                    <li><a href="product_list.php">Danh sach san pham</a></li>
+                </ul>
+            </li>
+        </ul>
+
+    </div>
